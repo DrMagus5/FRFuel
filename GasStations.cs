810c7c648ae9ad4be93e5e087945a9212adfc7f7
DrMagus5 <33024855+DrMagus5@users.noreply.github.com>
2017-10-23 12:52:52 +0200

    Expand the gastation information.
    I don't know if this can be usefull, I never used github. Anyway this changes could be interesting to expand the information of any gastation and make them unique. All cords must be added and also the CreateBlip() need to be changed in FrFuel.
    
    I changed the CreateBlip() in this way :
            /// <summary>
            /// Creates blips for gas stations
            /// </summary>
            public void CreateBlips()
            {
                if (Config.Get("CreateBlips", "true") != "true")
                {
                    return;
                }
    
                for (int i = 0; i < GasStations.fuelInfo.Length; i++)
                {
    
                    //HAS AT LEAST 3 ELEMENT
                    if (GasStations.fuelInfo[i].Count >= 3)
                    {
    
                        float _gasStationPositionX = float.Parse(GasStations.fuelInfo[i][0].ToString());
                        float _gasStationPositionY = float.Parse(GasStations.fuelInfo[i][1].ToString());
                        float _gasStationPositionZ = float.Parse(GasStations.fuelInfo[i][2].ToString());
    
                        var blip = World.CreateBlip(new Vector3(_gasStationPositionX, _gasStationPositionY, _gasStationPositionZ));
    
                        if (GasStations.fuelInfo[i].Count >= 4)
                        {
                            //4 ELEMENT
    
                            if (GasStations.fuelInfo[i][3].ToString() == "JerryCan")
                            {
                                blip.Sprite = BlipSprite.JerryCan;
                            }
    
                            //5 ELEMENT
    
                            if (GasStations.fuelInfo[i].Count >= 5)
                            {
                                blip.Name = GasStations.fuelInfo[i][4].ToString();
    
                                //6 ELEMENT
    
                                if (GasStations.fuelInfo[i].Count >= 6)
                                {
                                    if (GasStations.fuelInfo[i][5].ToString() == "White")
                                    {
                                        blip.Color = BlipColor.White;
                                    }
                                    else
                                    if (GasStations.fuelInfo[i][5].ToString() == "Blue")
                                    {
                                        blip.Color = BlipColor.Blue;
                                    }
    
                                    //7 ELEMENT
                                    if (GasStations.fuelInfo[i].Count >= 7)
                                    {
                                        blip.Scale = float.Parse(GasStations.fuelInfo[i][6].ToString());
    
                                        //8 ELEMENT
                                        if (GasStations.fuelInfo[i].Count >= 8)
                                        {
    
                                            if (GasStations.fuelInfo[i][7].ToString() == "true")
                                            {
                                                blip.IsShortRange = true;
                                            }
                                            else if (GasStations.fuelInfo[i][7].ToString() == "false")
                                            {
                                                blip.IsShortRange = false;
                                            }
    
                                            //9 ELEMENT
                                            if (GasStations.fuelInfo[i].Count >= 9)
                                            {
    
                                               // GasStations.fuelInfo[i][8].ToString()   PRICE EXPANSION FOR WHO NEED IT
      
    
                                            }
    
                                        }
    
    
                                     }
    
    
    
                                }
    
    
                            }
    
                        }
                        
    
                        blips[i] = blip;
                    }
    
    
                }
            }

--- GasStations.cs
+++ GasStations.cs
@@ -4,36 +4,13 @@ namespace FRFuel
 {
     public static class GasStations
     {
-        public static Vector3[] positions = {
-            new Vector3(49.41872f,   2778.793f,  58.04395f),
-            new Vector3(263.8949f,   2606.463f,  44.98339f),
-            new Vector3(1039.958f,   2671.134f,  39.55091f),
-            new Vector3(1207.26f,    2660.175f,  37.89996f),
-            new Vector3(2539.685f,   2594.192f,  37.94488f),
-            new Vector3(2679.858f,   3263.946f,  55.24057f),
-            new Vector3(2005.055f,   3773.887f,  32.40393f),
-            new Vector3(1687.156f,   4929.392f,  42.07809f),
-            new Vector3(1701.314f,   6416.028f,  32.76395f),
-            new Vector3(179.8573f,   6602.839f,  31.86817f),
-            new Vector3(-94.46199f,  6419.594f,  31.48952f),
-            new Vector3(-2554.996f,  2334.402f,  33.07803f),
-            new Vector3(-1800.375f,  803.6619f,  138.6512f),
-            new Vector3(-1437.622f, -276.7476f,  46.20771f),
-            new Vector3(-2096.243f, -320.2867f,  13.16857f),
-            new Vector3(-724.6192f, -935.1631f,  19.21386f),
-            new Vector3(-526.0198f, -1211.003f,  18.18483f),
-            new Vector3(-70.21484f, -1761.792f,  29.53402f),
-            new Vector3(265.6484f,  -1261.309f,  29.29294f),
-            new Vector3(819.6538f,  -1028.846f,  26.40342f),
-            new Vector3(1208.951f,  -1402.567f,  35.22419f),
-            new Vector3(1181.381f,  -330.8471f,  69.31651f),
-            new Vector3(620.8434f,   269.1009f,  103.0895f),
-            new Vector3(2581.321f,   362.0393f,  108.4688f),
-            new Vector3(1785.363f,   3330.372f,  41.38188f),
-            new Vector3(-319.690f,   -1471.610f,  30.030f), /* Innocence Blvd / Alta St [SR-19] */
-            new Vector3(174.880f,    -1562.450f,  28.740f), /* Davis Ave / Macdonald St */
-            new Vector3(1246.480f, -1485.450f,  34.900f), /* Near El Rancho Blvd [SR-18] / Capital Blvd */
-            new Vector3(-66.330f, -2532.570f,  6.140f) /* Near Miriam Turner Overpass */
+        
+        public static List<string>[] fuelInfo = {
+            // X , Y , Z , SpriteName, NameOnMap , Color , Scale , isshortrange , fuelPrice
+             new List<string>(new string[] { "49.41872", "2778.793", "58.04395", "JerryCan" ,"Gas Station", "White", "1" , "true","1"}),
+             new List<string>(new string[] { "263.8949", "2606.463", "44.98339", "JerryCan" ,"Gas Station", "Blue" , "2" , "true","1.2"}),
+            new List<string>(new string[] { "1039.958", "2671.134", "39.55091", "JerryCan" ,"Gas Station", "White", "1" , "true","1"}),
+             new List<string>(new string[] { "1207.26", "2660.175", "37.89996", "JerryCan" ,"Gas Station", "Blue" , "2" , "true","1.2"})
         };
 
         public static Vector3[][] pumps =
